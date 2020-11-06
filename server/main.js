5e7e05b66506a1bebf5b17c74cd1ef5a8841bd5f
Tan Khuu <tan.khuu@karrostech.com>
2020-11-06 18:50:35 +0700

    temporary fix to run in local

--- server/main.js
+++ server/main.js
@@ -13,51 +13,51 @@ Meteor.startup(function () {
   JobServer.start();
 
   // reprocess all jobs in server
-  let types = SLAs_Jobs.find({}, {fields: {type: true}})
-      .fetch()
-      .map(type => type.type)
-    ;
-  types = _.uniq(types);
-  types.map(type => {
-    if(type.split('-')[0] !== 'admin') {
-      console.log(`processing job: ${type}`);
-      SLAs_Jobs.processJobs(type, Workers.execute);
-    }
-  });
+  // let types = SLAs_Jobs.find({}, {fields: {type: true}})
+  //     .fetch()
+  //     .map(type => type.type)
+  //   ;
+  // types = _.uniq(types);
+  // types.map(type => {
+  //   if(type.split('-')[0] !== 'admin') {
+  //     console.log(`processing job: ${type}`);
+  //     SLAs_Jobs.processJobs(type, Workers.execute);
+  //   }
+  // });
   
-  // const adminUser = Accounts.users.findOne({})
-  /* Job cleanup indices */
-  if (Meteor.settings.admin.cleanup.jobs.enable) {
-    const {frequency: freqText} = Meteor.settings.admin.cleanup.jobs;
-    const type = 'admin-cleanupJobs';
-    const noJobs = SLAs_Jobs.find({type}).count();
+  // // const adminUser = Accounts.users.findOne({})
+  // /* Job cleanup indices */
+  // if (Meteor.settings.admin.cleanup.jobs.enable) {
+  //   const {frequency: freqText} = Meteor.settings.admin.cleanup.jobs;
+  //   const type = 'admin-cleanupJobs';
+  //   const noJobs = SLAs_Jobs.find({type}).count();
 
-    if(noJobs === 0) {
-      const
-        depends = [],
-        priority = 'normal',
-        retry = {},
-        repeat = {
-          schedule: later.parse.text(freqText)
-        },
-        delay = 0,
-        after = new Date();
+  //   if(noJobs === 0) {
+  //     const
+  //       depends = [],
+  //       priority = 'normal',
+  //       retry = {},
+  //       repeat = {
+  //         schedule: later.parse.text(freqText)
+  //       },
+  //       delay = 0,
+  //       after = new Date();
 
-      try {
-        const job = new Job(SLAs_Jobs, type, {});
-        job
-          .depends(depends)
-          .priority(priority)
-          .retry(retry)
-          .repeat(repeat)
-          .delay(delay)
-          .after(after)
-          .save()
-        ;
-      } catch (err) {
-        throw new Meteor.Error('CREATE_CLEANUP_JOBS', err.message);
-      }
-    }
-    SLAs_Jobs.processJobs(type, Workers.cleanupJobs);
-  }
+  //     try {
+  //       const job = new Job(SLAs_Jobs, type, {});
+  //       job
+  //         .depends(depends)
+  //         .priority(priority)
+  //         .retry(retry)
+  //         .repeat(repeat)
+  //         .delay(delay)
+  //         .after(after)
+  //         .save()
+  //       ;
+  //     } catch (err) {
+  //       throw new Meteor.Error('CREATE_CLEANUP_JOBS', err.message);
+  //     }
+  //   }
+  //   SLAs_Jobs.processJobs(type, Workers.cleanupJobs);
+  // }
 });
